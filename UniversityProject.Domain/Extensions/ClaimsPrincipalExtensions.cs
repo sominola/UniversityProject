3f7719728b6bca5ed5554eb3ac3168cfc9f130f3
sominola <nsavchuk224@gmail.com>
2022-01-16 03:40:54 +0200

    Create ClaimsPrincipalExtensions.cs

---
+++ UniversityProject.Domain/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using UniversityProject.Data.Entities;
+using UniversityProject.Data.Repositories.Interfaces;
+
+namespace UniversityProject.Domain.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static long GetCurrentUserId(this ClaimsPrincipal principal)
+    {
+        if (principal == null)
+        {
+            throw new ArgumentNullException(nameof(principal));
+        }
+
+        var claim = principal.Claims.First(c => c.Type == "id");
+        return long.Parse(claim.Value);
+    }
+}
