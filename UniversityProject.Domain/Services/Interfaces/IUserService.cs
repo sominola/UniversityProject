0df66d2eb5822dc0cc24d7686594d2a7f778f9c6
sominola <nsavchuk224@gmail.com>
2022-01-16 03:41:10 +0200

    Add methods

--- UniversityProject.Domain/Services/Interfaces/IUserService.cs
+++ UniversityProject.Domain/Services/Interfaces/IUserService.cs
@@ -5,4 +5,6 @@ namespace UniversityProject.Domain.Services.Interfaces;
 public interface IUserService
 {
     Task CreateUserAsync(RegisterUserDto model);
+    Task<UpdateUserDto> GetCurrentUserAsync();
+    Task UpdateUserAsync(UpdateUserDto model);
 }
