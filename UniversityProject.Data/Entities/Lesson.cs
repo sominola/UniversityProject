18956258bfb7ed88b660cd88108ee7438f60f94a
sominola <nsavchuk224@gmail.com>
2022-01-20 14:32:47 +0200

    Add Many to Many

--- UniversityProject.Data/Entities/Lesson.cs
+++ UniversityProject.Data/Entities/Lesson.cs
@@ -3,5 +3,6 @@
 public class Lesson: BaseEntity
 {
     public string Name { get; set; }
-    public List<User> Users = new();
+    public List<User> Students { get; set; } = new();
+    public List<User> Teachers { get; set; } = new();
 }
---
+++ UniversityProject.Data/Entities/LessonTeacher.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityProject.Data.Entities;
+
+public class LessonTeacher
+{
+    public long LessonId { get; set; }
+    public Lesson Lesson { get; set; }    
+    
+    public long TeacherId { get; set; }
+    public User Teacher { get; set; }
+}
