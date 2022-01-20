6959f82cc35d5fc37087755f1a64ac41a6c9e564
sominola <nsavchuk224@gmail.com>
2022-01-20 14:39:45 +0200

    Create JwtTokenMiddleware.cs

---
+++ UniversityProject.Web/Middlewares/JwtTokenMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace UniversityProject.Web.Middlewares;
+
+public class JwtTokenMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public JwtTokenMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var contains = context.Request.Cookies.TryGetValue("Token", out var token);
+        if (contains && !string.IsNullOrEmpty(token))
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+                context.Request.Headers.Add("Authorization", "Bearer " + token);
+        }
+        await _next.Invoke(context);
+    }
+}
