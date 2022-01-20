0934ae1bebcfb9d16db81427c998e7feef2fdd6a
sominola <nsavchuk224@gmail.com>
2022-01-20 14:34:15 +0200

    Move exceptions to another namespace

---
+++ UniversityProject.Domain/Exceptions/ApiResultException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace UniversityProject.Domain.Exceptions;
+
+public class ApiResultException: ResultException
+{
+    public ApiResultException(string message, HttpStatusCode code) : base(message, code)
+    {
+    }
+}
--- UniversityProject.Domain/Exceptions/ResultException.cs
+++ UniversityProject.Domain/Exceptions/ResultException.cs
@@ -5,13 +5,9 @@ namespace UniversityProject.Domain.Exceptions;
 public class ResultException: Exception
 {
     public readonly HttpStatusCode Code;
-    public readonly bool IsApiException;
-    public ResultException(string message, HttpStatusCode code):base(message)
+
+    protected ResultException(string message, HttpStatusCode code):base(message)
     {
         Code = code;
-    }
-    public ResultException(string message, HttpStatusCode code, bool isApiException):this(message,code)
-    {
-        IsApiException = isApiException;
     }
 }
