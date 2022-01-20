82461841fa6b53b8e352be92bbfad870e37815d9
sominola <nsavchuk224@gmail.com>
2022-01-20 14:34:29 +0200

    Update validators

---
+++ UniversityProject.Domain/FluentValidation/CreateLessonValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using UniversityProject.Domain.Dto.Lessons;
+
+namespace UniversityProject.Domain.FluentValidation;
+
+public class CreateLessonValidator:AbstractValidator<CreateLessonDto>
+{
+    public CreateLessonValidator()
+    {
+        RuleFor(x => x.Name).NotNull().NotEmpty();
+    }
+}
